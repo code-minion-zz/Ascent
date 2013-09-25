5fe6dc8dd9bd0ceaaa9ae3aa12318dbad7a2635f
manakpp <mana.kpp@gmail.com>
2013-09-26 11:35:14 +1200

    Put rigid bodies on the player and monsters. Started writing AI states.

---
+++ Ascent/Assets/Source/Monster/MAIState_Seek.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MAIState_Seek : MonsterAIState
+{
+    override public void Update()
+    {
+       
+    }
+
+    override public bool EvaluateEndCondition()
+    {
+        return true;
+    }
+}
