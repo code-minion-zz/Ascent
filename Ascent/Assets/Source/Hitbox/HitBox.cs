0b841cdbb971e69207fab362ecfa0bcb979968ef
Kit <chanwk3@gmail.com>
2013-09-26 13:23:57 +1200

    Starting Hitbox as part of Player::Attack()

---
+++ Ascent/Assets/Source/Hitbox/HitBox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.name.Contains("Player"))
+		{
+			isHit = true;	
+			Debug.Log("hit");
+		}		
+	}
+	
+	void OnCollisionExit(Collision collisionInfo)
+	{
+		isHit = false;		
+	}
+	
+	bool isHit
+	{
+		get{return isHit;}
+		set{;}
+	}
+}
