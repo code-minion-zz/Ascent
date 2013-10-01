980933d291a316c6e517a1bac57fb5b02e06132c
Mana <mana.kpp@gmail.com>
2013-10-01 21:53:09 +1300

    Restructed folders so that prefabs, textures and models are in the resources folder. Made basic visual debugger that should be easily extendable.

---
+++ Ascent/Assets/Source/AscentVisualDebugger/VisualDebugger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VisualDebugger : MonoBehaviour 
+{
+    private PlayerVDO players;
+    private MonsterVDO monsters;
+
+	void Start () 
+    {
+        players = gameObject.AddComponent<PlayerVDO>();
+        monsters = gameObject.AddComponent<MonsterVDO>();
+	}
+
+    void Update () 
+    {
+        if(Input.GetKeyUp(KeyCode.Alpha9))
+        {
+            players.Toggle();
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            monsters.Toggle();
+        }
+	}
+}
