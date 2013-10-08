36d28fde60e413d616b4ed34c138bfa16a13c186
Philip Roigard <philip.roigard@gmail.com>
2013-10-09 11:55:42 +1300

    Added physics materials for objects - Added new scene for level Editor - Began working on level editor

---
+++ Ascent/Assets/Editor/GridEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Ascent
+{
+    [CustomEditor(typeof(Grid))]
+    public class GridEditor : Editor
+    {
+        Grid grid;
+
+        public void OnEnable()
+        {
+            grid = target as Grid;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(" Grid Width ");
+            grid.width = EditorGUILayout.FloatField(grid.width, GUILayout.Width(50));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(" Grid Height ");
+            grid.height = EditorGUILayout.FloatField(grid.height, GUILayout.Width(50));
+            GUILayout.EndHorizontal();
+
+            SceneView.RepaintAll();
+        }
+    }
+}
