d9f8b6b9fbaafad3b0986a5bb7f082f8cb41520a
Mana <mana.kpp@gmail.com>
2013-10-30 14:30:14 +1300

    New scene to test all the hazards. Tried making a generic object pool. Updated the arrow shooter so that direction, arrow speed, frequency and damage can be changed in the editor.

--- Ascent/Assets/Source/Arrow.cs
+++ Ascent/Assets/Source/Arrow.cs
@@ -1,23 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Arrow : MonoBehaviour 
+public class Arrow : MonoBehaviour
 {
-    public float lifeSpan;
+    private float lifeSpan;
     private bool toDestroy = false;
+    private Vector3 direction;
+    private float speed;
+    private float damage;
 
 	// Use this for initialization
 	void Start () 
     {
 	
 	}
+
+    public void Initialise(float life, Vector3 direction, float speed, float damage)
+    {
+        lifeSpan = life;
+        toDestroy = false;
+        this.direction = direction;
+        this.speed = speed;
+        this.damage = damage ;
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
+        transform.position += direction * speed * Time.deltaTime;
         if(toDestroy)
         {
-            Object.Destroy(this.gameObject);
+           // Object.Destroy(this.gameObject);
+           gameObject.SetActive(false);
         }
 
         if (lifeSpan > 0.0f)
@@ -26,12 +40,15 @@ public class Arrow : MonoBehaviour
         }
         else
         {
-            Object.Destroy(this.gameObject);
+            gameObject.SetActive(false);
         }
 	}
 
     void OnCollisionEnter(Collision collision)
     {
+        // TODO: Deal damage to other object if it is a character
+        damage = damage + damage - damage; // suppress the warning;
+
         toDestroy = true;
     }
 }
--- Ascent/Assets/Source/Monster/ArrowShooter.cs
+++ Ascent/Assets/Source/Monster/ArrowShooter.cs
@@ -4,25 +4,51 @@ using System.Collections;
 public class ArrowShooter : MonoBehaviour 
 {
     public GameObject projectile;
-    public Vector3 direction;
+    public int projectilePoolCount = 5;
+    public float frequency = 1.0f;
+    public float projectileSpeed = 5.0f;
+    public float projectileDamage = 2.0f;
+
+    private ObjectPool arrowPool;
+    private Vector3 direction;
+    private Vector3 spawnPoint;
+    private float timeElapsed = 0.0f;
+    private float arrowLifeSpawn = 2.0f;
 
 	// Use this for initialization
 	void Start () 
     {
+        arrowPool = new ObjectPool(projectile, projectilePoolCount, this.transform, "Arrow");
+        direction = (transform.FindChild("Shooter").transform.position - transform.FindChild("Base").transform.position).normalized;
+        spawnPoint = transform.FindChild("Shooter").transform.position + (direction * 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        
-        if (Time.frameCount % 35 == 0)
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > frequency)
         {
+            timeElapsed -= frequency;
+
             // Instantiate an arrow
-            GameObject arrow = GameObject.Instantiate(projectile, transform.GetChild(1).transform.position +( direction * 1.0f), new Quaternion(0.0f, 0.0f, 0.0f, 1.0f)) as GameObject;
-
-            arrow.transform.parent = transform;
-
-            arrow.rigidbody.AddForce(direction * 50.0f);
+            ObjectPool.PoolObject po = arrowPool.GetInactive();
+            if (po != null)
+            {
+                Arrow newArrow = po.script as Arrow;
+                newArrow.Initialise(arrowLifeSpawn, direction, projectileSpeed, projectileDamage);
+                po.go.SetActive(true);
+                po.go.transform.position = spawnPoint;
+                po.go.transform.LookAt(new Vector3(1.0f, 0.0f, 0.0f));
+                //po.go.transform.tra
+                //po.go.rigidbody.velocity = Vector3.zero;
+                //po.go.rigidbody.angularVelocity = Vector3.zero;
+                //po.go.rigidbody.AddForce(direction * 50.0f);
+            }
+            else
+            {
+                Debug.Log(name + " ran out of " + projectile.ToString() + " to fire.");
+            }
         }
 	}
 }
