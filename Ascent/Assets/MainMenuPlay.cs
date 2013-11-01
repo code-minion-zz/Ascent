9a94cc09ac32f6e68f499ae9a7e4682216fd4da3
Kit <chanwk3@gmail.com>
2013-11-01 16:35:10 +1300

    Main menu now goes to floor.unity

---
+++ Ascent/Assets/MainMenuPlay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuPlay : MonoBehaviour {
+	
+	public string levelName;
+	
+	public void OnClick()
+	{
+		Application.LoadLevel("Floor");
+	}
+}
