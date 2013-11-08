8186ffdfa48943061d48f9ede8890d4e235d8b86
Mana <mana.kpp@gmail.com>
2013-11-08 16:13:25 +1300

    AI behaviours

---
+++ Ascent/Assets/Source/Character/Enemy/AI/DetectRay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Core;
+using RAIN.Action;
+
+[RAINAction]
+public class DectectRay : RAINAction
+{
+    public DectectRay()
+    {
+        actionName = "DectectRay";
+    }
+
+    public override void Start(AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(AI ai)
+    {
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(AI ai)
+    {
+        base.Stop(ai);
+    }
+}
