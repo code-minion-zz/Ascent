2a11a460f650cd6277f007b1d050c57edd119803
Mana <mana.kpp@gmail.com>
2013-11-10 23:32:31 +1300

    Gave the rat some reusable AI.

---
+++ Ascent/Assets/Source/Character/Enemy/AI/UseAbilityAction.cs
@@ -0,0 +1,32 @@
+﻿// Developed by Mana Khamphanpheng 2013
+
+// Dependencies
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Core;
+using RAIN.Action;
+
+[RAINAction]
+public class UseAbilityAction : RAINAction
+{
+	public UseAbilityAction()
+	{
+		actionName = "UseAbilityAction";
+	}
+
+	public override void Start(AI ai)
+	{
+		base.Start(ai);
+	}
+
+	public override ActionResult Execute(AI ai)
+	{
+		return ActionResult.SUCCESS;
+	}
+
+	public override void Stop(AI ai)
+	{
+		base.Stop(ai);
+	}
+}
