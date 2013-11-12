8b046c228f617b2d2f8d9d9e7cd2ff08479c0729
Langolier <chanwk3@gmail.com>
2013-11-13 00:00:24 +1300

    Charge Deals Damage

--- Ascent/Assets/Source/Character/Abilities/Charge.cs
+++ Ascent/Assets/Source/Character/Abilities/Charge.cs
@@ -8,11 +8,12 @@ public class Charge : Action
 {
 	private float timeElapsed = 0.0f;		// how long for
 	private float halfWayPoint = 0.2f;
+	//private float endChargeTime = 0.2f;
 	private float prevAnimatorSpeed = 0f;
 	private float actionSpeed = 15f;
 	
     private float distanceTraveled;
-	private float distanceMax = 12f;
+	private float distanceMax = 10f;
 	
 	private Animator ownerAnimator;
 	
@@ -30,7 +31,6 @@ public class Charge : Action
 	{
 		Reset ();
 		ownerAnimator.SetBool("SwingAttack",true);		// play anim
-		ownerAnimator.SetFloat("Speed", 5);
 		owner.ChargeBall.gameObject.SetActive(true);	
 		prevAnimatorSpeed = ownerAnimator.speed;
 	}
@@ -38,27 +38,38 @@ public class Charge : Action
     public override void UpdateAbility()
 	{
         Vector3 moveVec = owner.transform.forward * Time.deltaTime * actionSpeed;
-        owner.transform.position += moveVec;
-        distanceTraveled += moveVec.magnitude;
 		timeElapsed += Time.deltaTime;
 		
-		if (timeElapsed > halfWayPoint)
-		{
-			ownerAnimator.speed = 1f * timeElapsed; // freeze animation when swinging sword down
+		if (endCharge)
+		{			
+			owner.transform.position += moveVec * Time.deltaTime;
+			if (timeElapsed > 1-timeElapsed)
+			{
+				owner.StopAbility();
+			}
+			Debug.Log ("Ending Charge");
 		}
-
-		if (distanceTraveled >= distanceMax)
-		{
-			owner.StopAbility();
+		else
+		{			
+	        owner.transform.position += moveVec;
+	        distanceTraveled += moveVec.magnitude;
+			
+			if (timeElapsed > halfWayPoint)
+			{
+				ownerAnimator.speed = 0.1f * timeElapsed; // freeze animation when swinging sword down
+			}
+	
+			if (distanceTraveled >= distanceMax)
+			{
+				EndCharge();
+			}
 		}
 	}
 
     public override void EndAbility()
 	{	owner.ChargeBall.gameObject.SetActive(false);
 		ownerAnimator.speed = prevAnimatorSpeed;
-		//ownerAnimator.StopPlayback();
 		ownerAnimator.SetBool("SwingAttack",false);
-		ownerAnimator.SetFloat("Speed", 0.9f);
 	}
 	
 	private void Reset()
@@ -68,66 +79,22 @@ public class Charge : Action
 		endCharge = false;
 	}
 	
-	private void OnHitWall(GameObject go)
+	private void OnHitWall(Character other)
 	{
-		owner.StopAbility();
+		EndCharge();
 	}
 	
-	private void OnHitEnemy(GameObject go)
+	private void OnHitEnemy(Character other)
 	{
-		owner.StopAbility();
+		EndCharge();
+		other.ApplyDamage(10,Character.EDamageType.Physical);
+		other.ApplyKnockback(Vector3.up,100f);
+	}
+	
+	private void EndCharge()
+	{
+		endCharge = true;
+		timeElapsed = 0f;
+		ownerAnimator.speed = 0.8f;
 	}
 }
-
-/*
-// Developed by Kit Chan 2013
-
-// Dependencies
-using UnityEngine;
-using System.Collections;
-
-public class Charge : IAction 
-{
-	Character owner;
-	private const float animationTime = 2.333f;
-	private const float animationSpeed = 2.0f;
-	private float timeElapsed;
-    //private float distanceMax = 20.0f;
-    //private float distanceTraveled;
-
-    public void Initialise(Character owner)
-    {
-        this.owner = owner;
-    }
-    
-	public void StartAbility()
-	{
-		timeElapsed = 0.0f;
-		owner.Animator.PlayAnimation("SwingAttack");
-		
-        // get owner's charge box and enable collision
-		owner.Weapon.EnableCollision = true;
-		owner.Weapon.SetAttackProperties(999,Character.EDamageType.Physical);
-	}
-
-	public void UpdateAbility()
-	{
-//
-//        Vector3 moveVec = owner.transform.forward * Time.deltaTime * animationSpeed;
-//        distanceTraveled += moveVec.magnitude;
-//        Debug.Log(distanceTraveled);
-//        owner.transform.position += moveVec;
-//
-//		if (distanceTraveled >= distanceMax)
-//		{
-//			owner.StopAbility();
-//		}
-	}
-
-	public void EndAbility()
-	{
-		owner.Weapon.EnableCollision = false;
-		owner.Animator.StopAnimation("SwingAttack");
-	}
-}
-*/
