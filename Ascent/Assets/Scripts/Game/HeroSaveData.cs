aaa5d9a2e5158768b427a3e08083ad8a2763a0f4
Mana <mana.kpp@gmail.com>
2013-11-28 18:54:28 +1300

    Simple backend for game saving.

---
+++ Ascent/Assets/Scripts/Game/HeroSaveData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroSaveData 
+{
+	public int level;
+	public Character.EHeroClass type;
+	public float saveTime;
+	public ulong id;
+	// Inventory
+	// Abilities
+	// Experience 
+	public Data manage;
+	public struct Data
+	{
+		//public int goldCount;
+		//public int floorLevel;
+		//public int gameTime;
+		//public int maxHealth;
+		//public int maxSpecial;
+	}
+
+	public HeroSaveData()
+	{
+		saveTime = Time.time;
+		//manage.goldCount = 1;
+		//manage.floorLevel = 1;
+		//manage.gameTime = 1;
+		//manage.maxHealth = 1;
+		//manage.maxSpecial = 1;
+		level = 1;
+	}
+
+	public override string ToString()
+	{
+		string toString = "HeroSave\nTime: " + saveTime + "\n" +
+							"ID: " + id;
+
+		return toString;
+	}
+}
---
+++ Ascent/Assets/Scripts/Game/HeroSaveDataList.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class HeroSaveDataList  
+{
+	[XmlArray("HeroSaves")]
+	[XmlArrayItem("HeroSaveData")]
+	public List<HeroSaveData> heroSaves = new List<HeroSaveData>();
+}
