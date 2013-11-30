6f3c89420ca36ce56c933eb01b55999c0b041c72
Kit <chanwk3@gmail.com>
2013-11-30 13:52:33 +1300

    wip, bits and pieces

--- Ascent/Assets/Scripts/GUI/UIMenu.cs
+++ Ascent/Assets/Scripts/GUI/UIMenu.cs
@@ -2,11 +2,31 @@ using UnityEngine;
 
 using System;
 
+/// <summary>
+/// A Visible Menu (or Scene State)
+/// </summary>
 public class UIMenu : MonoBehaviour
 {
 	UIPlayerPanel				activePanel;
 	BetterList<UIPlayerPanel> 	playerPanels;
 	
 	public delegate void PanelTransitionEvent(UIPlayerPanel destination);
+
+	/// <summary>
+	/// Perform all processes to deactivate a Menu
+	/// </summary>
+	public void Deactivate()
+	{
+		this.gameObject.SetActive(false);
+	}
+
+	public void Activate()
+	{
+		this.gameObject.SetActive(true);
+	}
+
+	protected void PlayAnimation(bool reverse = false)
+	{
+	}
 }
 
