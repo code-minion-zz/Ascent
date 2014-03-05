49831dc99505ddda31de09d828b62dcacd0c5985
Mana <mana.kpp@gmail.com>
2014-03-05 14:36:38 +1300

    Rooms can have their sizes set so the camera will be fixed in them.

--- Ascent/Assets/Scripts/Projectiles/FallingDebris.cs
+++ Ascent/Assets/Scripts/Projectiles/FallingDebris.cs
@@ -21,33 +21,31 @@ public class FallingDebris : Projectile
 
     public void OnCollisionEnter(Collision collision)
     {
-        // TODO: DAMAGE IN AREA
+        //// Check what it has collided with
+        //Layer layer = (Layer)collision.gameObject.layer;
 
-        // Check what it has collided with
-        Layer layer = (Layer)collision.gameObject.layer;
-
-        switch (layer)
-        {
-            case Layer.Environment:
-            case Layer.Floor:
-                {
-                    // Blow up
-                    Debug.Log("Hit Environment/Floor");
-                }
-                break;
-            case Layer.Hero:
-            case Layer.Monster:
-                {
-                    // Blow up and deal damage to this unit
-                    Debug.Log("Hit Character");
-                }
-                break;
-            default:
-                {
-                    Debug.LogError("Unhandled case, " + layer.ToString() + ", " + collision.collider.gameObject.name);
-                }
-                break;
-        }
+        //switch (layer)
+        //{
+        //    case Layer.Environment:
+        //    case Layer.Floor:
+        //        {
+        //            // Blow up
+        //            Debug.Log("Hit Environment/Floor");
+        //        }
+        //        break;
+        //    case Layer.Hero:
+        //    case Layer.Monster:
+        //        {
+        //            // Blow up and deal damage to this unit
+        //            Debug.Log("Hit Character");
+        //        }
+        //        break;
+        //    default:
+        //        {
+        //            Debug.LogError("Unhandled case, " + layer.ToString() + ", " + collision.collider.gameObject.name);
+        //        }
+        //        break;
+        //}
 
 
         GameObject explosion = GameObject.Instantiate(Resources.Load("Prefabs/Projectiles/DebrisExplosion")) as GameObject;
