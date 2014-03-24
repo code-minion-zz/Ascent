106a621b0d6ddf8fe58a53b5316aec20fa590511
Kit <chanwk3@gmail.com>
2014-03-24 15:51:16 +1300

    starting sound manager

---
+++ Ascent/Assets/AscentSoundManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AscentSoundManager : MonoBehaviour {
+
+	void Start()
+	{		
+		audio.clip = Resources.Load("Sound");
+	}
+
+	public void SetSound()
+	{
+	}
+}
