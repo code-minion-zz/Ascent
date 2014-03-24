0a2241177735709dc6e29f99eef37e7f47cb91e5
Mana <mana.kpp@gmail.com>
2014-03-25 00:35:51 +1300

    Made a triggers and actions system with some starting triggers and actions for room event type stuff like doors closing and opening. Shouldn't be able to push MovableBlock blocks into walls or push the hero out either anymore. Tweaked the blade so it was 1 unit scale which makes more sense for the public length field in the script. Created outline for a floor and populated 4 of the rooms with content.

---
+++ Ascent/Assets/Scripts/Tower/Environment/OpenDoorAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoorAction : EnvironmentAction 
+{
+	public Doors doors;
+
+	private bool initialised;
+
+	void OnEnable()
+	{
+		initialised = true;
+	}
+
+	public override void ExecuteAction()
+	{
+		if (initialised)
+		{
+			foreach (Door d in doors.RoomDoors)
+			{
+				d.OpenDoor();
+			}
+			//initialised = false;
+		}
+
+	}
+}
