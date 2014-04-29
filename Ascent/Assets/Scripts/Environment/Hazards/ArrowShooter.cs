48d0943288e00f833b1be2b1525f85e76b6d874e
Mana <mana.kpp@gmail.com>
2014-04-29 15:41:49 +1200

    Updated the 2nd level with correct assets.

--- Ascent/Assets/Scripts/Environment/Hazards/ArrowShooter.cs
+++ Ascent/Assets/Scripts/Environment/Hazards/ArrowShooter.cs
@@ -72,6 +72,7 @@ public class ArrowShooter : EnvironmentHazard
 
 				if (Physics.Raycast(new Ray(position, Direction * 1.0f), out hitInfo, 0.50f, layerMask))
                 {
+                    Debug.Log("a");
                     return;
                 }
 
